28491b70e1b21a9db37f0cb0d9a285ec4a7ef9f9
Carlgo11 <github@carlgo11.com>
2016-02-07 21:00:46 +0100

    initial commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php return array(
+
+    # database settings
+    'mysql-host' => 'localhost',
+    'mysql-username' => 'greenbuttons',
+    'mysql-password' => 'xWMBYUFbSAGdCrPz',
+    'mysql-database' => 'greenbuttons',
+    'mysql-table' => 'redirects',
+    
+    # Directory containing files saved the server
+    'file-dir' => 'files',
+    
+    # Amount of buttons to load on the page.
+    'button-amount' => 1000,
+);?>
